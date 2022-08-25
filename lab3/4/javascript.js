93122249d415466378dffa632e56405a33850438
ilovemhuukrob <kanlayakorn.yeenang@gmail.com>
2022-08-25 21:42:58 +0700

    เสือเมี้ยว

---
+++ lab3/4/javascript.js
@@ -0,0 +1,16 @@
+function viewchart() {
+    document.getElementById("mathpercent").style.animation = "math 3s";
+    document.getElementById("mathpercent").style.width = "30vw";
+
+    document.getElementById("thaipercent").style.animation = "thai 3s";
+    document.getElementById("thaipercent").style.width = "40vw";
+
+    document.getElementById("engpercent").style.animation = "eng 3s";
+    document.getElementById("engpercent").style.width = "25vw";
+
+    document.getElementById("scipercent").style.animation = "sci 3s";
+    document.getElementById("scipercent").style.width = "20vw";
+
+    document.getElementById("socialpercent").style.animation = "social 3s";
+    document.getElementById("socialpercent").style.width = "25vw";
+}
