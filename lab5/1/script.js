1d2b52c1baa87ce1816202c0a752dfb988130dd0
ilovemhuukrob <kanlayakorn.yeenang@gmail.com>
2022-09-06 15:29:52 +0700

    เสือเมี้ยว

---
+++ lab5/1/script.js
@@ -0,0 +1,17 @@
+let requestURL = 'data.json';
+let request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+        ExtractData(JSON.parse(request.responseText));
+    }
+};
+request.open("GET", requestURL, true);
+request.send();
+
+function ExtractData(data) {
+    const one = 1;
+const two = 2;
+    for (let i = 0; i < data.members.length; i++) {
+        document.write(`${i+1} <b> ${data.members[i].firstname} ${data.members[i].lastname} </b> (${data.members[i].gender}) is a ${data.members[i].position}, ${data.members[i].address}<br>`);
+    }
+}
